12872a947b44a1a77c37580fb0b69c6fd7411d54
patrobinson81 <patrobinson81@gmail.com>
2014-09-01 20:31:16 -0400

    colors

--- config/defaults.js
+++ config/defaults.js
@@ -15,7 +15,7 @@ define([], function () {
         "color": "#ffffff",
         //Theme defines the background color of the title bar and also the background color of the popup
         //title bar. 
-        "theme": "#5F5F5F",
+        "theme": "#e33d3d",
         //Specify the color used for the closeable drawer titles.  
         "paneltheme": "#ededed",
         "bingmapskey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
