540b558bb9e07dfde1efb3191b95d272faedf7d4
patrobinson81 <patrobinson81@gmail.com>
2014-09-01 20:18:56 -0400

    Webmap

--- config/defaults.js
+++ config/defaults.js
@@ -1,10 +1,10 @@
-define([], function() {
+define([], function () {
     //Default configuration settings for the application. This is where you'll define things like a bing maps key,
     //default web map, default app color theme and more. These values can be overwritten by template configuration settings
     //and url parameters.
     var defaults = {
         "appid": "",
-        "webmap": "6c4e81f90c4b4935945e9b623c5247e0",
+        "webmap": "48fe49e79da4459d9356d4b22fc06cc6",
         "oauthappid": null, //"AFTKRmv16wj14N3z",
         //Group templates must support a group url parameter. This will contain the id of the group. 
         //group: "",
@@ -20,15 +20,15 @@ define([], function() {
         "paneltheme": "#ededed",
         "bingmapskey": "", //Enter the url to your organizations bing maps key if you want to use bing basemaps
         //Defaults to arcgis.com. Set this value to your portal or organization host name.
-        "sharinghost": location.protocol + "//" + "www.arcgis.com", 
+        "sharinghost": location.protocol + "//" + "www.arcgis.com",
         //When true the template will query arcgis.com for default settings for helper services, units etc. If you 
         //want to use custom settings for units or any of the helper services set queryForOrg to false then enter
         //default values for any items you need using the helper services and units properties. 
-        "queryForOrg": true, 
-        "units": null, 
+        "queryForOrg": true,
+        "units": null,
         //Enter text here that will display in the title section. 
         //If not specified the web map title is used. 
-        "title": null, 
+        "title": null,
         //Enter text here that will display in the about panel 
         //If not specified the web map description is used. 
         //When true the locate button is displayed on the map. 
@@ -38,18 +38,18 @@ define([], function() {
         "home": true,
         //When true the geocoder search box is displayed in the title area
         "search": true,
-        "helperServices": {  
-           "geometry":{
-            "url": null
-           },
-           "printTask": {
-            "url": null
-           },
-           "elevationSync":{
-             "url": null
-           },
-           "geocode": [{
-            "url": null
+        "helperServices": {
+            "geometry": {
+                "url": null
+            },
+            "printTask": {
+                "url": null
+            },
+            "elevationSync": {
+                "url": null
+            },
+            "geocode": [{
+                "url": null
            }]
         }
     };
